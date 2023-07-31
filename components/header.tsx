ce1a27a72c6447b30a1ada2c8975f405142e47ab
aonosorah <102835599+aonosorah@users.noreply.github.com>
2023-07-31 12:28:48 -0300

    responsivo

--- components/header.tsx
+++ components/header.tsx
@@ -10,7 +10,8 @@ export default function HeaderTop() {
       <header className="flex flex-1 w-full p-10 justify-between" id="top">
         <button
           onClick={() => push('/')}
-          className="flex justify-center gap-3 items-center bg-white w-36 h-10 border border-solid border-4 rounded-full border-[#546E4D]  max-md:w-14 max-md:h-14"
+          className="flex justify-center gap-3 items-center bg-white w-36 h-10 border 
+          border-solid border-4 rounded-full border-[#546E4D]  max-md:w-16 max-md:h-14"
         >
           {' '}
           <Image width={20} height={10} src={'barras.svg'} alt="" />{' '}
@@ -27,14 +28,14 @@ export default function HeaderTop() {
             width={197}
             height={200}
             src={'petlogo.svg'}
-            className="absolute top-0 w-48 h-52 left-20 max-md:w-{118px} max-md:h-{120}"
+            className="absolute top-0 w-48 h-52 left-20 max-md:w-{118px} max-md:h-{120} max-md:left-10"
             alt=""
           />
         </div>
 
         <button
           onClick={() => push('/pettdating')}
-          className="flex justify-center gap-3 items-center bg-[#546E4D] text-white w-36 h-10 border border-solid border-4 rounded-full border-[#546E4D] max-md:w-14 max-md:h-14 "
+          className="flex justify-center gap-3 items-center bg-[#546E4D] text-white w-36 h-10 border border-solid border-4 rounded-full border-[#546E4D] max-md:w-16 max-md:h-14 "
         >
           {' '}
           <Image width={15} height={15} src={'enter.svg'} alt="" />
